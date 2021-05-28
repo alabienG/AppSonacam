8e826c9678659a242e6c3249d65bfcb8da1f0d89
Gouchere Georges <31745977+alabienG@users.noreply.github.com>
2021-05-28 11:48:16 +0100

    Initial commit

---
+++ AppSonacam/src/test/java/com/stampicorp/AppSonacam/AppSonacamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stampicorp.AppSonacam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSonacamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
